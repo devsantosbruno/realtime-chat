041e761b95ed586de7b45cf1c330ca629bbc9ab8
bruno santos <devbrunosantos@gmail.com>
2024-06-06 23:28:24 -0300

    feat: Avatar components

---
+++ src/app/components/Avatar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { User } from "@prisma/client";
+import Image from "next/image";
+
+type AvatarProps = {
+	user?: User;
+};
+
+export function Avatar({ user }: AvatarProps) {
+	return (
+		<div className="relative">
+			<div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+				<Image
+					alt="Avatar"
+					src={user?.image ?? "/images/placeholder.png"}
+					fill
+				/>
+			</div>
+
+			<span className="absolute top-0 right-0 h-2 w-2 md:h-3 md:w-3 block rounded-full bg-green-500 border-2 border-white" />
+		</div>
+	);
+}
