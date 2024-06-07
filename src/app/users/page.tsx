6a6242754e138191921df70f37b78c2ebc2390a0
bruno santos <devbrunosantos@gmail.com>
2024-06-06 23:27:32 -0300

    feat: Users page

---
+++ src/app/users/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { EmptyState } from "../components";
+
+export default function Users() {
+	const session = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (session.status === "unauthenticated") {
+			router.push("/login");
+		}
+	}, [session, router]);
+
+	return (
+		<div className="hidden lg:block lg:pl-80 h-full">
+			<EmptyState />
+		</div>
+	);
+}
