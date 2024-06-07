58c00f198e9c36b31256c5988d0d3ee95ae3db6f
bruno santos <devbrunosantos@gmail.com>
2024-06-06 23:28:53 -0300

    chore: exporting components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,5 @@
+export { Avatar } from "./Avatar";
 export { Button } from "./Button";
+export { EmptyState } from "./EmptyState";
 export { Input } from "./Inputs/Input";
+export { Sidebar } from "./Sidebar";
