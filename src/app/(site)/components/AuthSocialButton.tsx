436b90252df4e6c8b45744536ee7ffca11de841f
bruno santos <devbrunosantos@gmail.com>
2024-06-08 20:42:22 -0300

    style: login page

--- src/app/(site)/components/AuthSocialButton.tsx
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -7,12 +7,11 @@ type AuthSocialButtonProps = {
 
 export function AuthSocialButton({ Icon, onClick }: AuthSocialButtonProps) {
 	return (
-		<button
-			type="button"
-			onClick={onClick}
-			className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm hover:bg-gray-50 transition duration-500"
-		>
-			<Icon />
+		<button type="button" onClick={onClick} className="inline-flex w-fit">
+			<Icon
+				size={24}
+				className="text-slate-50 hover:text-lime-400 transition duration-500"
+			/>
 		</button>
 	);
 }
--- src/app/components/Inputs/Input.tsx
+++ src/app/components/Inputs/Input.tsx
@@ -30,7 +30,7 @@ export function Input({
 		<div>
 			<label
 				htmlFor={id}
-				className="block text-sm font-medium leading-6 text-gray-900"
+				className="block text-sm font-light leading-6 text-slate-50"
 			>
 				{label}
 			</label>
@@ -43,7 +43,7 @@ export function Input({
 					disabled={disabled}
 					{...register(id, { required })}
 					className={clsx(
-						"form-input block w-full rounded-md py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 border border-gray-300 focus:border-sky-600 transition duration-500",
+						"form-input bg-transparent block w-full rounded-md py-1.5 text-slate-50 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 border border-gray-800 focus:border-lime-400 focus:ring-0 focus:outline-none transition duration-500",
 						errors[id] && "focus:border-rose-500",
 						disabled && "opacity-50 cursor-default",
 					)}
