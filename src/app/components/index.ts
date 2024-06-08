5ae4f252105be80251c992467072df19ca1b6a34
bruno santos <devbrunosantos@gmail.com>
2024-06-07 22:43:16 -0300

    feat: AvatarGroup component and exporting

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,8 @@
 export { Avatar } from "./Avatar";
+export { AvatarGroup } from "./AvatarGroup";
 export { Button } from "./Button";
 export { EmptyState } from "./EmptyState";
 export { Input } from "./Inputs/Input";
+export { LoadingModal } from "./LoadingModal";
 export { Modal } from "./Modal";
 export { Sidebar } from "./Sidebar";
