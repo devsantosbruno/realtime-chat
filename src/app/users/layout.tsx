4bd9b53490a8308a463180a24f97d8afa6cb71ab
bruno santos <devbrunosantos@gmail.com>
2024-06-07 21:15:30 -0300

    feat: users layout

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,10 +1,15 @@
+import { getUsers } from "@/app/actions";
 import { Sidebar } from "@/app/components/Sidebar";
+import { UserList } from "./components/UserList";
 
 export default async function UsersLayout({
 	children,
 }: { children: React.ReactNode }) {
+	const users = await getUsers();
+
 	return (
 		<Sidebar>
+			<UserList items={users} />
 			<div className="h-screen">{children}</div>
 		</Sidebar>
 	);
