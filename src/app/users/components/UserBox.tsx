1f6d58e034c85caf4ffa150915a6a9e304416ef3
bruno santos <devbrunosantos@gmail.com>
2024-06-07 22:52:00 -0300

    feat: UserBox using Loading component

--- src/app/users/components/UserBox.tsx
+++ src/app/users/components/UserBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar } from "@/app/components";
+import { Avatar, LoadingModal } from "@/app/components";
 import type { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -26,17 +26,21 @@ export function UserBox({ data }: UserBoxProps) {
 	}, [data, router]);
 
 	return (
-		<div onClick={handleClick}>
-			<div className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
-				<Avatar user={data} />
-				<div className="min-w-0 flex-1">
-					<div className="focus:outline-none">
-						<div className="flex justify-between items-center mb-1">
-							<p className="text-sm font-medium text-gray-900">{data.name}</p>
+		<>
+			{isLoading && <LoadingModal />}
+
+			<div onClick={handleClick}>
+				<div className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
+					<Avatar user={data} />
+					<div className="min-w-0 flex-1">
+						<div className="focus:outline-none">
+							<div className="flex justify-between items-center mb-1">
+								<p className="text-sm font-medium text-gray-900">{data.name}</p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
