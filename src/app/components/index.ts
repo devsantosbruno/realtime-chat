3da5d54fbe71f04bcaa522af684bf2497b477bc5
bruno santos <devbrunosantos@gmail.com>
2024-06-07 21:12:26 -0300

    feat: Modals components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,4 +2,5 @@ export { Avatar } from "./Avatar";
 export { Button } from "./Button";
 export { EmptyState } from "./EmptyState";
 export { Input } from "./Inputs/Input";
+export { Modal } from "./Modal";
 export { Sidebar } from "./Sidebar";
